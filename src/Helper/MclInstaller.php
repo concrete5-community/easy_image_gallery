ff2d9b773c1153f41df3b7a08870e69fae02107a
Jacqmin Sébastien <seb@tellthem.be>
2017-02-22 13:45:34 +0100

    cleaning mclInstaller

--- src/Helper/MclInstaller.php
+++ src/Helper/MclInstaller.php
@@ -5,23 +5,6 @@ use Concrete\Core\Backup\ContentImporter;
 
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
-use Page;
-use Package;
-use SinglePage;
-use PageType;
-use BlockType;
-use Block;
-use PageTheme;
-use Loader;
-use Core;
-use PageTemplate;
-use CollectionAttributeKey;
-use FileAttributeKey;
-use UserAttributeKey;
-use \Concrete\Core\Block\BlockType\Set as BlockTypeSet;
-use \Concrete\Core\Attribute\Type as AttributeType;
-use \Concrete\Core\Attribute\Key\Category as AttributeKeyCategory;
-
 class MclInstaller extends ContentImporter
 {
 
